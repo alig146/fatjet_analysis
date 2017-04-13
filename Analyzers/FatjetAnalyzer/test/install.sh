6b57505d7b08e588ec11d184b5508e4c82757011
Tote <tote@physics.rutgers.edu>
2017-04-12 21:50:27 -0500

    bug

--- Analyzers/FatjetAnalyzer/test/install.sh
+++ Analyzers/FatjetAnalyzer/test/install.sh
@@ -11,8 +11,8 @@ git clone git@github.com:elliot-hughes/decortication.git
 #mkdir ../python
 #cd ../python
 cd $CMSSW_BASE/python
-ln -s $CMSSW_BASE/src/Deracination/test/decortication/decortication/ decortication
-ln -s $CMSSW_BASE/src/Deracination/test/truculence/truculence/ truculence
+ln -s $CMSSW_BASE/src/Deracination/Straphanger/test/decortication/decortication/ decortication
+ln -s $CMSSW_BASE/src/Deracination/Straphanger/test/truculence/truculence/ truculence
 
 cd $CMSSW_BASE/src
 echo "[..] Compiling CMSSW area."
