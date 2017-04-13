069b4804f626dc3e8127aec0a71ec3edae3b1e14
Tote <tote@physics.rutgers.edu>
2017-04-12 21:43:33 -0500

    fixing where stuff goes

--- Analyzers/FatjetAnalyzer/test/install.sh
+++ Analyzers/FatjetAnalyzer/test/install.sh
@@ -8,10 +8,11 @@ cd $CMSSW_BASE/src/Deracination/Straphanger/test
 echo "[..] Fetching python packages."
 git clone git@github.com:elliot-hughes/truculence.git
 git clone git@github.com:elliot-hughes/decortication.git
-mkdir ../python
-cd ../python
-ln -s ../test/decortication/decortication/ decortication
-ln -s ../test/truculence/truculence/ truculence
+#mkdir ../python
+#cd ../python
+cd $CMSSW_BASE/python
+ln -s $CMSSW_BASE/src/Deracination/test/decortication/decortication/ decortication
+ln -s $CMSSW_BASE/src/Deracination/test/truculence/truculence/ truculence
 
 cd $CMSSW_BASE/src
 echo "[..] Compiling CMSSW area."
