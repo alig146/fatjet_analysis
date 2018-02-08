33d9afa3f788d17e260b5d42251aff9e79b66f3e
Elliot Hughes <tote@physics.rutgers.edu>
2018-02-08 17:18:03 -0500

    updated the update script

--- Analyzers/FatjetAnalyzer/test/update_fatjet.sh
+++ Analyzers/FatjetAnalyzer/test/update_fatjet.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "[..] Updating the analysis code." &&
 cd $CMSSW_BASE/src &&
 git pull origin master &&
 cd $CMSSW_BASE/src/Deracination/Straphanger/test/truculence &&
 git pull origin master &&
 cd $CMSSW_BASE/src/Deracination/Straphanger/test/decortication && 
-git pull origin	master
+git pull origin	master &&
+echo "[OK] The analysis code was updated successfully."
 
